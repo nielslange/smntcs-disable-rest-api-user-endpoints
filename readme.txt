c5fbdbfd4db078fa04fe39ef65832fdd178db884
Niels Lange <nielslange@nemesis-2.local>
2016-12-24 10:58:35 +0700

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,29 @@
+=== SMNTCS Disable REST API User Endpoints ===
+Contributors: nielslange
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=QL92KESGMVT4Q
+Tags: REST API, Endpoints, Security
+Requires at least: 4.7
+Tested up to: 4.7
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Disable the REST API user endpoints due to obscure user slugs
+
+== Description ==
+
+With WordPress 4.7 the REST API is part of the core. At the moment everyone has read access to the REST API. As a result of that a potential intruder can retrieve a list of all user slugs via /wp-json/wp/v2/users. This plugin disables the REST API user endpoints to obscure the user slugs.
+
+== Installation ==
+
+1. Upload 'smntcs-disable-rest-api-user-endpoints' to the '/wp-content/plugins/' directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Screenshots ==
+
+Simple activate the plugin and you're done.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
---
+++ smntcs-disable-rest-api-endpoints.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ Plugin Name: SMNTCS Disable REST API User Endpoints
+ Description: Disable the REST API user endpoints due to security reasons
+ Version: 1.0
+ Author: Niels Lange
+ Author URI: https://nielslange.com
+ Text Domain: smntcs-disable-rest-api-user-endpoints
+ Domain Path: /languages/
+ */
+
+/*  Copyright 2014-2016	Niels Lange (email : info@nielslange.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+// Avoid direct plugin access
+if ( !defined( 'ABSPATH' ) ) exit;
+
+// Load text domain
+add_action('plugins_loaded', function() {
+	load_plugin_textdomain( 'smntcs-disable-rest-api-user-endpoints', false, dirname( plugin_basename(__FILE__) ) . '/languages/' );
+});
+
+// Disable REST API user endpoints
+add_filter( 'rest_endpoints', function( $endpoints ){
+	if ( isset( $endpoints['/wp/v2/users'] ) ) {
+		unset( $endpoints['/wp/v2/users'] );
+	}
+	if ( isset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] ) ) {
+		unset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] );
+	}
+	return $endpoints;
+});
